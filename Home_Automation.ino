6df163e48682f1d5e6d61809e656a866f51f33ff
jeetamar28 <101172727+jeetamar28@users.noreply.github.com>
2023-09-02 02:03:19 +0530

    Update Home_Automation.ino

--- Home_Automation.ino
+++ Home_Automation.ino
@@ -3,10 +3,10 @@
 #include <BlynkSimpleEsp8266.h>
 #define BLYNK_TEMPLATE_ID "TMPL3-aQfbF9C"
 #define BLYNK_TEMPLATE_NAME "Home Automation"
-#define BLYNK_AUTH_TOKEN "aJ0SP26FYgYbdzXr_VwYgiC21ZcsVdWD"
+#define BLYNK_AUTH_TOKEN "****P26F***YbdzXr_VwYg****1Zcs*****"
 
-char ssid[] = "DESKTOP-51ACM2M 0304"; // username of WI-FI
-char pass[] = "28902077"; // password of Wi-Fi
+char ssid[] = "DESKTOP-5CM2M"; // username of WI-FI
+char pass[] = "00000000"; // password of Wi-Fi
 
 void setup()
 {
